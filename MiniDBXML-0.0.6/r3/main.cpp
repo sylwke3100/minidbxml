0247d217bd8355b8922918a02bde0a2941928cb1
sylwek3100@gmail.com <sylwek3100@gmail.com@82d84734-1910-aefd-4d80-768b69274859>
2011-01-21 23:33:50 +0000

    Update Structure folders

--- MiniDBXML-0.0.6/r3/main.cpp
+++ MiniDBXML-0.0.6/r3/main.cpp
@@ -3,7 +3,7 @@
 #include <string>
 #include <fstream>
 #include "./version.h"
-#include "./security.h"
+#include "./modules/security.h"
 #include <conio.h>
 
 using namespace std;
--- MiniDBXML-0.0.6/r3/version.h
+++ MiniDBXML-0.0.6/r3/version.h
@@ -2,36 +2,32 @@
 #define VERSION_H
 
 namespace AutoVersion{
-
+	
 	//Date Version Types
-	static const char DATE[] = "19";
+	static const char DATE[] = "22";
 	static const char MONTH[] = "01";
 	static const char YEAR[] = "2011";
-	static const char UBUNTU_VERSION_STYLE[] = "10.12";
-
+	static const char UBUNTU_VERSION_STYLE[] = "11.01";
+	
 	//Software Status
 	static const char STATUS[] = "Alpha";
 	static const char STATUS_SHORT[] = "a";
-
+	
 	//Standard Version Type
 	static const long MAJOR = 0;
 	static const long MINOR = 0;
 	static const long BUILD = 6;
-	static const long REVISION = 462;
-
+	static const long REVISION = 464;
+	
 	//Miscellaneous Version Types
 	static const long BUILDS_COUNT = 1718;
-	#define RC_FILEVERSION 0,0,6,462
-	#define RC_FILEVERSION_STRING "0, 0, 6,462,\0"
-	static const char FULLVERSION_STRING[] = "0.0.6.426";
-
-	//SVN Version
-	static const char SVN_REVISION[] = "19";
-	static const char SVN_DATE[] = "2011-01-19T18:42:40.444701Z";
-
+	#define RC_FILEVERSION 0,0,6,464
+	#define RC_FILEVERSION_STRING "0, 0, 6, 464\0"
+	static const char FULLVERSION_STRING[] = "0.0.6.464";
+	
 	//These values are to keep track of your versioning state, don't modify them.
-	static const long BUILD_HISTORY = 26;
-
+	static const long BUILD_HISTORY = 0;
+	
 
 }
 #endif //VERSION_H
