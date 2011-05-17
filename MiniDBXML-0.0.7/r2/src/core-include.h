09736ed00b5e7066ff349b19f6796a4badc7f7cf
sylwek3100@gmail.com <sylwek3100@gmail.com@82d84734-1910-aefd-4d80-768b69274859>
2011-05-17 16:24:05 +0000

    -Add: Configuration file -Fixed: Search function (copy 2x founded entry)

--- MiniDBXML-0.0.7/r2/src/core-include.h
+++ MiniDBXML-0.0.7/r2/src/core-include.h
@@ -1,3 +1,40 @@
+<<<<<<< .mine
+/*
+MiniDBXML -Core Include (core-include.h)
+
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+ */
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <fstream>
+#include <conio.h>
+#include <cstring>
+#include <sys/prctl.h>
+#include <signal.h>
+#include <dirent.h>
+#include <errno.h>
+#include <vector>
+#include <sstream>
+#include "./version.h"
+#include "./core-prefs.cpp"
+//Install Modules
+
+#include "./modules/security.h"
+#include "./modules/multidb.cpp"
+=======
 /*
 MiniDBXML -Core Include (core-include.h)
 
@@ -33,3 +70,4 @@ MiniDBXML -Core Include (core-include.h)
 
 #include "./modules/security.h"
 #include "./modules/multidb.h"
+>>>>>>> .r91
--- MiniDBXML-0.0.7/r2/src/version.h
+++ MiniDBXML-0.0.7/r2/src/version.h
@@ -1,3 +1,42 @@
+<<<<<<< .mine
+#ifndef VERSION_H
+#define VERSION_H
+
+namespace AutoVersion{
+	
+	//Date Version Types
+	static const char  _DATE[] = "04";
+	static const char  _MONTH[] = "05";
+	static const char  _YEAR[] = "2011";
+	static const char  _UBUNTU_VERSION_STYLE[] = "11.05";
+	
+	//Software Status
+	static const char  _STATUS[] = "Alpha";
+	static const char  _STATUS_SHORT[] = "a";
+	
+	//Standard Version Type
+	static const long  _MAJOR = 0;
+	static const long  _MINOR = 0;
+	static const long  _BUILD = 0;
+	static const long  _REVISION = 7;
+	
+	//Miscellaneous Version Types
+	static const long  _BUILDS_COUNT = 2876;
+	#define  _RC_FILEVERSION 0,0,0,7
+	#define  _RC_FILEVERSION_STRING "0, 0, 0, 7\0"
+	static const char  _FULLVERSION_STRING[] = "0.0.0.7";
+	
+	//SVN Version
+	static const char  _SVN_REVISION[] = "0";
+	static const char  _SVN_DATE[] = "unknown date";
+	
+	//These values are to keep track of your versioning state, don't modify them.
+	static const long  _BUILD_HISTORY = 1;
+	
+
+}
+#endif //VERSION_H
+=======
 #ifndef VERSION_H
 #define VERSION_H
 
@@ -25,3 +64,4 @@ namespace AutoVersion{
 
 }
 #endif //VERSION_H
+>>>>>>> .r91
