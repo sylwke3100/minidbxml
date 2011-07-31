d11f8c239b0d74f3fab1616c3fa5a4c57e9d828b
sylwek3100 <sylwek3100@82d84734-1910-aefd-4d80-768b69274859>
2011-07-31 12:21:29 +0000

    Add main file

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cstdlib>
+#include <vector>
+#include "DbControl.h"
+#include "XmlParser.h"
+#include "DbSearch.h"
+#include "UiCommandParser.h"
+using namespace std;
+
+
+
+int main()
+{
+
+  /*  cout<<"Ladowanie bazy\n";
+    DB * base = new DB("a.xml");
+    cout<<"Ladowanie bazy2\n";
+    cout<<base->GetEntryById(1,0);
+    vector <string> Elo;
+    DbSearch Szukaj;
+    Szukaj.SetSearchValue("s");
+    Szukaj.SearchInDb(base,Elo);
+    cout<<"Szukanie\n";
+    cout<<"Wyniki wyszukiwania:\n";
+    for(int i=0; i<Elo.size(); i++)
+    {
+        cout<<Elo[i]<<"\n";
+    }
+*/
+    UiCommandParser Text;
+    string str;
+    while(getline(cin,str))
+    {
+        Text.ParseCommand(str);
+    }
+}
